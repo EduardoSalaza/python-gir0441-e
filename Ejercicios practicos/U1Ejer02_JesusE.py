41402821cd3477773f8c636f7cf79115ad394f08
EduardoSalaza <106277151+EduardoSalaza@users.noreply.github.com>
2022-10-14 23:12:21 -0500

    Add files via upload

---
+++ Ejercicios practicos/U1Ejer02_JesusE.py
@@ -0,0 +1,20 @@
+'''
+Act:Manejo de listas
+Autor: Jesus Eduardo Salazar Venegas 
+'''
+
+#Se mostrara un mensaje de Bienvenida
+
+print("Hola te Mostraremos la lista de Nombres")
+
+#Agregraremos las ecuaciones correspondiente 
+
+nombres = ['Francisco','pedro','juan','fernando']
+['Francisco','Joaquin','juan','fernando','Luis miguel']
+nombres.append("gustavo")
+for nombres in nombres:
+  print(nombres)
+
+# Se agregara una despedida
+
+print("Esta es la lsita de los Usuarios ¡Adios!")
